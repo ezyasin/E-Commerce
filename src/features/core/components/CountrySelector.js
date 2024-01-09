875785a149f1ac0a3cf678cf2f614c8175343c87
Yasin <f201213059@ktun.edu.tr>
2024-01-09 14:23:21 +0300

    feat: Country selector component

---
+++ src/features/core/components/CountrySelector.js
@@ -0,0 +1,45 @@
+import { useState } from "react";
+
+const countries = [
+  { id: 0, name: "Select a country" },
+  { id: 1, name: "USA" },
+  { id: 2, name: "Canada" },
+  { id: 3, name: "Mexico" },
+  { id: 4, name: "United Kingdom" },
+  { id: 5, name: "France" },
+  { id: 6, name: "Germany" },
+  { id: 7, name: "Russia" },
+  { id: 8, name: "Japan" },
+  { id: 9, name: "China" },
+  { id: 10, name: "South Korea" },
+  { id: 11, name: "Turkey" },
+];
+
+const CountrySelector = () => {
+  const [selectedCountry, setSelectedCountry] = useState(countries[0]);
+
+  const handleChange = (event) => {
+    setSelectedCountry(countries[event.target.value]);
+    onSelect(countries[event.target.value]);
+  };
+
+  return (
+    <div className="flex flex-col">
+      <select
+        name="country"
+        id="country"
+        className="mt-1 p-2 border rounded"
+        onChange={handleChange}
+        value={selectedCountry.id}
+      >
+        {countries.map((country) => (
+          <option value={country.id} key={country.id}>
+            {country.name}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+};
+
+export default CountrySelector;
